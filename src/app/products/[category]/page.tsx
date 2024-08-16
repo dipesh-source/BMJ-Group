ef3d7887ccf9c026ec1dd7d75af5b7c21acad81b
Dipesh Parmar <71171310+dipesh-source@users.noreply.github.com>
2024-08-16 10:19:37 +0530

    Update page.tsx

--- src/app/products/[category]/page.tsx
+++ src/app/products/[category]/page.tsx
@@ -1,34 +1,34 @@
-// import Image from "next/image";
-// import ProductCategoryView from "@/sections/product/product-view"
+import Image from "next/image";
+import ProductCategoryView from "@/sections/product/product-view"
 
-// import { Metadata } from 'next'
+import { Metadata } from 'next'
  
-// export const metadata: Metadata = {
-//   title: 'Drinks',
-// }
-
-// export default function ProductPage() {
-//   return <ProductCategoryView />
-// }
-
-
-
-import { Metadata } from 'next';
-import ProductCategoryView from '@/sections/product/product-view';
-
 export const metadata: Metadata = {
   title: 'Drinks',
-};
-
-// Define the possible category paths to pre-generate
-export async function generateStaticParams() {
-  const categories = ["wine", "beer", "whiskey"]; // Static example
-
-  return categories.map((category) => ({
-    category: category.toLowerCase(), // Ensure consistency
-  }));
 }
 
 export default function ProductPage() {
-  return <ProductCategoryView />;
+  return <ProductCategoryView />
 }
+
+
+
+// import { Metadata } from 'next';
+// import ProductCategoryView from '@/sections/product/product-view';
+
+// export const metadata: Metadata = {
+//   title: 'Drinks',
+// };
+
+// // Define the possible category paths to pre-generate
+// export async function generateStaticParams() {
+//   const categories = ["wine", "beer", "whiskey"]; // Static example
+
+//   return categories.map((category) => ({
+//     category: category.toLowerCase(), // Ensure consistency
+//   }));
+// }
+
+// export default function ProductPage() {
+//   return <ProductCategoryView />;
+// }
